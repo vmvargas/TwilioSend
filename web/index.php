29ada9808e2ab1b765be1fb9a6c76cfe3aa47540
vmvargas <victor07vargas@gmail.com>
2017-10-02 18:57:21 -0400

    debugging test2, 2 sms are sent

--- web/index.php
+++ web/index.php
@@ -11,11 +11,8 @@ $to = '19172445030';
 $from = '19172677334';
 $message_body = 'Hello from Victor Vargas CS 643 Fall 2017';
 
-$client->messages->create($to,
-    array(
-        'from' => $from,
-        'body' => $message_body
-    )
-);
-
-echo "An SMS was sent to " + $to + " from " + $from;
+$messages = $client->accounts($sid)
+  ->messages->create($to, array(
+        'From' => $from,
+        'Body' => $message_body,
+  ));
