279d01426d13abdaa248d82f563783b603b9ebb9
vmvargas <victor07vargas@gmail.com>
2017-10-02 19:02:36 -0400

    debugging test3, 2 sms are sent

--- web/index.php
+++ web/index.php
@@ -1,18 +1,14 @@
 <?php
 
-require('../vendor/autoload.php');
+require_once '../vendor/autoload.php'; // Loads the library
 use Twilio\Rest\Client;
 
-$sid = 'ACa4f3cd8fc3f81e9affc4990d28142e80';
-$token = '30030ba27276e7fbe2fa41e2b390f22d';
-$client = new Client($sid, $token);
+$account_sid = 'ACa4f3cd8fc3f81e9affc4990d28142e80';
+$auth_token = '30030ba27276e7fbe2fa41e2b390f22d';
+$client = new Client($account_sid, $auth_token);
 
-$to = '19172445030';
-$from = '19172677334';
-$message_body = 'Hello from Victor Vargas CS 643 Fall 2017';
-
-$messages = $client->accounts($sid)
-  ->messages->create($to, array(
-        'From' => $from,
-        'Body' => $message_body,
+$messages = $client->accounts("ACa4f3cd8fc3f81e9affc4990d28142e80")
+  ->messages->create("+19172445030", array(
+        'From' => "+19172677334",
+        'Body' => "Hello from Victor Vargas CS 643 Fall 2017",
   ));
