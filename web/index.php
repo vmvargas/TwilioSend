36ea1a4ad73903ed6b2096cd185f78939b6e3c84
vmvargas <victor07vargas@gmail.com>
2017-10-01 22:46:48 -0400

    test 2 to see if sms is sent

--- web/index.php
+++ web/index.php
@@ -7,13 +7,14 @@ $sid = 'ACa4f3cd8fc3f81e9affc4990d28142e80';
 $token = '30030ba27276e7fbe2fa41e2b390f22d';
 $client = new Client($sid, $token);
 
-$client->messages->create(
-    // the number you'd like to send the message to
-    '+19172445030',
+$to = '19172445030';
+$from = '19172677334';
+$message_body='Hello from Victor Vargas CS 643 Fall 2017';
+
+$client->messages->create($to,
     array(
-        // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+19172677334',
-        // the body of the text message you'd like to send
-        'body' => 'Hello from Victor Vargas CS 643 Fall 2017'
+        'from' => $from,
+        'body' => $message_body
     )
 );
+echo "An SMS was sent to " + $to + " from " + $from;
