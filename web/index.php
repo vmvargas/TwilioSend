f7717d02bf8f81fcbcf3da57ef2142961704fa8b
vmvargas <victor07vargas@gmail.com>
2017-10-03 23:02:57 -0400

    updating number to 1 917 349 0168

--- web/index.php
+++ web/index.php
@@ -8,11 +8,11 @@ $token = '30030ba27276e7fbe2fa41e2b390f22d';
 $client = new Client($sid, $token);
 
 $client->messages->create(
-    '19172445030',
+    '19173490168',
     array(
         'from' => '19172677334',
         'body' => 'Hello from Victor Vargas CS 643 Fall 2017'
     )
 );
 
-echo "SMS sent";
+echo "SMS sent to (917)349-0168";
