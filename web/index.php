4427b638b0bf32e0ba7f83f321893bf43f290027
vmvargas <victor07vargas@gmail.com>
2017-10-03 23:00:38 -0400

    updating phone number

--- web/index.php
+++ web/index.php
@@ -1,20 +1,16 @@
 <?php
 
-require('../vendor/autoload.php');
+require_once '../vendor/autoload.php';
 use Twilio\Rest\Client;
 
 $sid = 'ACa4f3cd8fc3f81e9affc4990d28142e80';
 $token = '30030ba27276e7fbe2fa41e2b390f22d';
 $client = new Client($sid, $token);
 
-// Use the client to do fun stuff like send text messages!
 $client->messages->create(
-    // the number you'd like to send the message to
-    '19173490168',
+    '19172445030',
     array(
-        // A Twilio phone number you purchased at twilio.com/console
         'from' => '19172677334',
-        // the body of the text message you'd like to send
         'body' => 'Hello from Victor Vargas CS 643 Fall 2017'
     )
 );
